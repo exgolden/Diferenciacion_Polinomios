6e22ea1a1f7fe538cd5ed15961b35020702e2c14
Isai <isai_alejandro@outlook.com>
2022-07-06 00:19:40 -0500

    Modificacion de la matriz asoaciada

--- DiferentialLT.py
+++ DiferentialLT.py
@@ -2,15 +2,17 @@ import numpy as np
 #Input
 print("Ingrese los coeficientes de su polinomio, incluidos los elementos 0 \n En la forma: a,a1,a2...,an")
 Coeff=str(input("Coeficientes: "))
-Coeff_l=Coeff.split(",")
+Coeff=np.array(Coeff.split(","), int)
 # Matriz Asociada
-Mat=np.zeros((len(Coeff_l), len(Coeff_l)), dtype=int)
-for i in range(len(Coeff_l)):
-    for j in range(len(Coeff_l)):
-        Mat[i][i]=Coeff_l[i]
-Row=np.zeros(len(Coeff_l), dtype=int)
-Mat=np.append(Mat, [Row], axis=0)
+INV=np.arange(len(Coeff)-1,0,-1)
+MAT=np.zeros((len(Coeff)-1, len(Coeff)-1), dtype=int)
+for i in range(len(INV)):
+    for j in range(len(INV)):
+        MAT[i][i]=INV[i]
+Col=np.zeros(len(INV), dtype=int)
+MAT=np.column_stack((MAT,Col))
 #Producto
-Coeff_n=np.matmul(Mat, np.array(Coeff_l, dtype=int), dtype=int)
+Coeff_n=np.matmul(MAT, Coeff, dtype=int)
 print("Los coeficienets de la derivada del polinomio son:")
-print(Coeff_n[0:-1])
+print(Coeff_n)
+
